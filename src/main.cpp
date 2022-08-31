28648b468876b23610a8a42b300da451935c437a
Hugge05 <75117937+Hugge05@users.noreply.github.com>
2022-08-31 14:08:30 +0200

    added namspace Lib
    Includes template print

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
-int main()
+#include <iostream>
+
+namespace Lib 
 {
 
+	template <typename T>
+	void Print(T t) {
+		std::cout << t << "\n";
+	}
+
+
+}
+
+
+
+
+int main()
+{
+	Lib::Print(123);
 }
